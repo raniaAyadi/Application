9150acf3b77828f65702125a6c91c73f1dd199f9
zouari <mustapha.zouari@bordeaux-inp.fr>
2018-07-02 14:50:18 +0200

    up

--- beenov/public/javascripts/component/infoSheetsItem.js
+++ beenov/public/javascripts/component/infoSheetsItem.js
@@ -2,7 +2,22 @@ function InfoSheetsItem(obj){
   Item.call(this, obj);
   this.titre = "Les Fiches";
   this.label = "Glisser et déposer des fiches depuis le panier pour en ajouter";
+  this.infoSheets = obj.infoSheets ? obj.infoSheets : new Array();
 }
 
 InfoSheetsItem.prototype = Object.create(Item.prototype);
 InfoSheetsItem.prototype.constructor = InfoSheetsItem;
+
+InfoSheetsItem.prototype.addInfoSheet = function(resource){
+  var obj = {
+    resource : resource
+  };
+
+  this.infoSheets.push(obj);
+};
+
+InfoSheetsItem.prototype.removeInfoSheet = function(resource){
+  this.infoSheets = this.infoSheets.filter(
+    elt => elt.resource != resource
+  );
+}
