c8077ec67ba7ad731dd7a7f0af661bade462cc56
zouari <mustapha.zouari@bordeaux-inp.fr>
2018-07-02 12:25:38 +0200

    image

--- beenov/public/javascripts/component/imageItem.js
+++ beenov/public/javascripts/component/imageItem.js
@@ -1,7 +1,12 @@
 function ImageItem(obj){
   Item.call(this, obj);
   this.label = "";
+  this.fileName = obj.fileName ? obj.fileName : null;
 }
 
 ImageItem.prototype = Object.create(Item.prototype);
 ImageItem.prototype.constructor = ImageItem;
+
+ImageItem.prototype.setFileName = function(name){
+  this.fileName = "upload_"+name;
+}
