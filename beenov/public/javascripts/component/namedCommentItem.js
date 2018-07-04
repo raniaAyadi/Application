8791294c6911ce736f77e2109e698f430bb04c03
camill_a <antoine.camille@epitech.eu>
2018-07-04 14:56:03 +0200

    a

--- beenov/public/javascripts/component/namedCommentItem.js
+++ beenov/public/javascripts/component/namedCommentItem.js
@@ -1,22 +1,20 @@
 function NamedCommentItem(obj){
   Item.call(this, obj);
-  // this.setComment(obj.name);
-  this.comment = "içi on voit le commentaire";
-console.log(this.comment);
-console.log("named comment");
+  this.setComment(obj.name);
+  //this.comment = "içi on voit le commentaire";
+
+
 }
 
 NamedCommentItem.prototype = Object.create(Item.prototype);
 NamedCommentItem.prototype.constructor = NamedCommentItem;
 
-// NamedCommentItem.prototype.setComment = function (name){
-//   this.comment = {
-//     name : name,
-//     text : "içi on voit le commentaire"
-//   };
-  //
-  // Meeting.getCurrentMeeting().done(()=>{
-  //   var meeting = Meeting.instance.object;
-  //   this.comment = meeting.getCommentByName(name); //renvoie le nom et le text de commentaire
-  //};
-// };
+
+NamedCommentItem.prototype.setComment = function (name){
+    this.comment = "";
+
+  Meeting.getCurrentMeeting().done(()=>{
+    var meeting = Meeting.instance.object;
+    this.comment = meeting.quiz.globalVariableValues[name]; //renvoie le nom et le text de commentaire
+  });
+};
