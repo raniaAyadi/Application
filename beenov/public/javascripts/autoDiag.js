1d34fc971419fca80a3539f97abd54bf56ef251e
zouari <mustapha.zouari@bordeaux-inp.fr>
2018-07-17 14:12:29 +0200

    done

---
+++ beenov/public/javascripts/autoDiag.js
@@ -0,0 +1,37 @@
+var AutoDiag = {
+  guestSubmit : new Event("guestSubmit")
+};
+
+AutoDiag.checkUrl = function(){
+  var url = window.location.href;
+  var params = url.split('/');
+  var data = {};
+
+  data.idUser = params[params.length - 2];
+  data.idQuiz = params[params.length - 1];
+
+  $.when(User.getById(data.idUser), Quiz.getById(data.idQuiz)).done((user, quiz) => {
+    AutoDiag.advisor = new User(user);
+    AutoDiag.quiz = new Quiz(quiz);
+    
+  }).fail(()=>{
+    alert("Vérifiez l'URL");
+    window.location.href = CONST.url.login;
+  });
+};
+
+AutoDiag.displayQuiz = function(){
+  var guestFormC = document.querySelector("beenov-guest-form");
+  guestFormC.remove();
+
+  var quizElt = document.querySelector("#quiz");
+  quizElt.removeAttribute("style");
+
+  var report = document.querySelector("#beenov-report");
+  report.append(new ReportComponent());
+};
+
+$(document).ready(function(){
+  AutoDiag.checkUrl();
+  document.body.addEventListener("guestSubmit", AutoDiag.displayQuiz);
+});
---
+++ beenov/public/javascripts/component/guestForm.js
@@ -0,0 +1,36 @@
+function GuestForm() {
+  this.company = {};
+}
+
+GuestForm.prototype.submit = function(siret){
+  var deferred = $.Deferred();
+
+  Company.setCompany(siret).done(()=>{
+    this.company = Company.getCurrentCompany();
+    localStorage.removeItem("company");
+
+    this.company.save(false).done(() => {
+      var quiz = AutoDiag.quiz;
+      Quiz.addEmptyQuiz(this.company.id, quiz.id).done((data)=>{
+        var cookie = {
+          quest: "questionnaires/" + quiz.id,
+          questRep : "questionnaire-replies/"+data.id
+        };
+        document.cookie = "infomet=" + JSON.stringify(cookie);
+
+        var me = this;
+        var infoCompany = {
+          companies : me.company.id,
+          reply : me.company.infoQuestionnaireReply
+        };
+
+        document.cookie = "company_info=" + JSON.stringify(infoCompany);
+        deferred.resolve();
+
+      }).fail(data => deferred.reject(data));
+    });
+
+  }).fail((data) => deferred.reject(data));
+
+  return deferred;
+};
