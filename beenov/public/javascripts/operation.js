24d9b5248d89451cb9764d18330ec0452f7d0eb7
camill_a <antoine.camille@epitech.eu>
2018-07-18 09:35:11 +0200

    guess fomr

--- beenov/public/javascripts/operation.js
+++ beenov/public/javascripts/operation.js
@@ -18,6 +18,30 @@ var Operation = {
       return null;
   },
 
+  checkNumber : function(e){
+    console.log("aaa");
+    // Allow: backspace, delete, tab, escape, enter and .
+if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+    // Allow: Ctrl+C
+    (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+    // Allow: Ctrl+X
+    (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39) ||
+    //Allow numbers and numbers + shift key
+    ((e.shiftKey && (e.keyCode >= 48 && e.keyCode <= 57)) || (e.keyCode >= 96 && e.keyCode <= 105))) {
+    // let it happen, don't do anything
+    return;
+  }
+// Ensure that it is a number and stop the keypress
+  if ((!e.shiftKey && (e.keyCode < 48 || e.keyCode > 57)) || (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+},
+
+
   convertObjectToArray : function(obj){
     var tab = new Array();
     for(var i in obj){
